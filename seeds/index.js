2e9e3e141549eed337b5941a6c797e042f208177
Calvin Chew <github@newzealot.com>
2020-11-18 17:18:55 +0800

    Added basic styles part 1

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: 'https://source.unsplash.com/collection/280662/1600x900',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eos est asperiores quos repellat atque fugit esse voluptas, facere blanditiis ducimus. Corrupti ipsa ab eum doloremque laborum placeat, possimus aliquam.',
+      price,
     });
     await camp.save();
   }
